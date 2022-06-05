6b61c7c92e8321cc6f35844278d1b7eb09c2c3df
Robson-William <95246442+Robson-William@users.noreply.github.com>
2022-06-04 21:29:57 -0300

    Adicionando questões 8 a 10 do livro de BM e descrição da questão 05 do livro de CBB

---
+++ BMExercicios/BMExercicio09/BMExercicio09.java
@@ -0,0 +1,9 @@
+// Escreva um programa que implemente um laço while que execute 20 vezes, imprimindo o valor da variável x que inicialmente está com valor 10
+
+class BMExercicio09 {
+	public static void main(String[] args){
+		Repetir repete = new Repetir();
+		
+		repete.repetirNumero();
+	}
+}
--- CBBExercicios/CBBAtividadePratica05/CBBAtividadePratica05.java
+++ CBBExercicios/CBBAtividadePratica05/CBBAtividadePratica05.java
@@ -1,3 +1,5 @@
+// Faça um programa que imprima os números pares de 0 a 100.
+
 class CBBAtividadePratica05 {
 	public static void main(String[] args){
 		NumerosPares par = new NumerosPares();
